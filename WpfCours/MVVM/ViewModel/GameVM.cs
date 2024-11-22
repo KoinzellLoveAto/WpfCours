093b26819b9b1d71c21db9e2a4c1f4e1021be37e
GISBERT Baptiste <46565128+KoinzellLoveAto@users.noreply.github.com>
2024-11-22 10:10:07 +0100

    wpfcours

---
+++ WpfCours/MVVM/ViewModel/GameVM.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfCours.MVVM.ViewModel
+{
+    public class GameVM : BaseVM
+    {
+        public ICommand RequestChangeViewCommand { get; set; }  
+        public GameVM() 
+        {
+            RequestChangeViewCommand = new RelayCommand(HandleRequestChangeViewCommand);
+        }
+        
+        public void HandleRequestChangeViewCommand()
+        {
+            MainWindowVM.OnRequestVMChange?.Invoke(new MainViewVM());
+        }
+
+        public override void OnShowVM()
+        {
+            MessageBox.Show("Gameview show");
+        }
+    }
+}
---
+++ WpfCours/MVVM/ViewModel/MainViewVM.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WpfCours.MVVM.View;
+
+namespace WpfCours.MVVM.ViewModel
+{
+    public class MainViewVM : BaseVM
+    {
+        public ICommand RequestChangeViewCommand { get; set; }
+        public MainViewVM()
+        {
+            RequestChangeViewCommand = new RelayCommand(HandleRequestChangeViewCommand);
+        }
+
+        public void HandleRequestChangeViewCommand()
+        {
+            MainWindowVM.OnRequestVMChange?.Invoke(new GameVM());
+        }
+
+        public override void OnShowVM()
+        {
+            MessageBox.Show("Main view display");
+        }
+    }
+}
